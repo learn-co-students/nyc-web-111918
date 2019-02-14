9a7989575f5886a3c0ab21ee041c71408e7c9583
Alex Griffith <alexalexgriffith@gmail.com>
2019-02-14 08:49:09 -0500

    Add 45 starter code

---
+++ 45-intro-to-redux/src/index.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Counter />
+      </div>
+    );
+  }
+}
+
+class Header extends Component {
+  render() {
+    return (
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Welcome to React</h1>
+      </header>
+    );
+  }
+}
+
+class Counter extends Component {
+  state = { count: 0 };
+
+  increment = () => {
+    this.setState(prevState => ({ count: prevState.count + 1 }));
+  };
+
+  decrement = () => {
+    this.setState(prevState => ({ count: prevState.count - 1 }));
+  };
+
+  renderDescription = () => {
+    const count = this.state.count
+    const remainder = count % 5;
+    const upToNext = 5 - remainder;
+    return `The current count is less than ${count + upToNext}`;
+  };
+
+  render() {
+    return (
+      <div className="Counter">
+        <h1>{this.state.count}</h1>
+        <button onClick={this.decrement}> - </button>
+        <button onClick={this.increment}> + </button>
+        <h3>{this.renderDescription()}</h3>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
